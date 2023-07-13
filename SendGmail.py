c4fa21f879b59f22d8edb619a80181ab6ff5bb9a
GabrielaPMRS <gabrielasoares.ec@gmail.com>
2023-07-13 15:32:50 -0300

    Adicionar funcionalidade de enviar email
    É possível enviar mensagens por email

---
+++ SendGmail.py
@@ -0,0 +1,28 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+
+
+# Os emails de comunicação sairão sempre do gmail da empresa e ele não admite respostas.
+# Todos os emails devem conter o email pessoal do empregados caso ele deseje continuar a conversa.
+email_sender = 'noreply.workhive@gmail.com'
+email_passward = 'glapmivmumcddcce'
+
+email_receiver = input('Email do destinatario: ')
+
+subject = input('Qual o assunto do seu email? ')
+print('\nLembre de informar um email pessoal para futuro contato com o prestador de serviço.\n')
+body = input('Digite sua mensagem: ')
+
+#criar uma instancia
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_passward)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
