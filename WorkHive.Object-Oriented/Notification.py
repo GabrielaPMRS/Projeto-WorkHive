87136ed0c30d7edd3a5a229c0f68056473c4a5b4
GabrielaPMRS <gabrielasoares.ec@gmail.com>
2023-08-29 20:24:27 -0300

    Adicionar o encapsulamento dos atributos da classe Notification usando o decorator @property

--- WorkHive.Object-Oriented/Notification.py
+++ WorkHive.Object-Oriented/Notification.py
@@ -3,5 +3,11 @@ class Notification():
         self._username = username
         self._feedback = feedback
 
- 
+    @property
+    def username(self):
+        return self._username
+    
+    @property
+    def feedback(self):
+        return self._feedback
     
